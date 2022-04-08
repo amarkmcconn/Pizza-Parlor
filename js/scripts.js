701130643a23a9e669b2ce75f0dcc01e25eef8ee
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-08 14:53:43 -0700

    getPrice values arent equalling business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ Pizza.prototype.getPrice = function() {
   if (this.pizzaSize === "medium") {
     if (this.toppings === "cheese") {
       return price + 6;
-    } else if (this.toppings === ("cheese", "pepperoni")) {
+    } else if (this.toppings === ["cheese", "pepperoni"]) {
       return price + 7;
     } else {
       return price + 8;
@@ -13,7 +13,7 @@ Pizza.prototype.getPrice = function() {
   } else if (this.pizzaSize === "large")  {
     if (this.toppings === "cheese") {
       return price + 11;
-    } else if (this.toppings === ("cheese", "pepperoni")) {
+    } else if (this.toppings === ["cheese", "pepperoni"]) {
       return price + 12;
     } else {
       return price + 13;
@@ -21,7 +21,7 @@ Pizza.prototype.getPrice = function() {
   } else if (this.pizzaSize === "xlarge") {
     if (this.toppings === "cheese") {
       return price + 15;
-    } else if (this.toppings === ("cheese", "pepperoni")) {
+    } else if (this.toppings === ["cheese", "pepperoni"]) {
       return price + 16;
     } else {
       return price + 17;
@@ -41,19 +41,33 @@ $(document).ready(function(){
   $("form#pizzaOrder").submit(function(event) {
     event.preventDefault();
     const pizzaSize = $("#pizzaSize").val();
-    // if (document.querySelector("#topping1").checked); {
+    let ele = [];
+    $("input:checkbox[name=topping]:checked").each(function() {
+      ele.push($(this).val());
+    })
+    let myPizza = new Pizza(pizzaSize, ele)
+    let myPizzaPrice = myPizza.getPrice();
+    console.log(myPizzaPrice);
+    
+  });
+});    
+
+
+
+// [(document.querySelector("#topping1").checked), (document.querySelector("#topping2").checked), (document.querySelector("#topping3").checked)])
+
+// if (document.querySelector("#topping1").checked); {
     //   topping1 = "cheese"  
     // if (document.querySelector("#topping2").checked);
     //   topping2 = "pepperoni"  
     // if (document.querySelector("#topping2").checked);
     //   topping3 = "pinneapple"
     // }
-    let myPizza = new Pizza(pizzaSize, [(document.querySelector("#topping1").checked), (document.querySelector("#topping2").checked), (document.querySelector("#topping3").checked)])
-    let myPizzaPrice = myPizza.getPrice();
-    console.log(myPizzaPrice);
     // displayPizzaOrder(myPizza);
-  });
-});    
 
 
+
+    // let topping1 = $("#topping1").is("checked:");
+    // let topping2 = $("#topping2").is("checked:");
+    // let topping3 = $("#topping3").is("checked:");
 
